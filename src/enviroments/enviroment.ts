f507016306041a5ea8ba18d9320d93031d21a25d
daniel palacio <palaciodaniel275@gmail.com>
2025-10-12 01:33:54 -0500

    bueno al principio se configuro los recurso de trerraform asociados a cloudfront y el bucket de s3 para poder subir a produccion el frontend, se crearon las vistas y servicios para conectar a user-api y card-api ademas de sus diferentes vistas

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  endpoints: {
+    users: 'https://jbxnp8dlk3.execute-api.us-east-1.amazonaws.com/prod',
+    cards: 'https://x8ewzbrf6k.execute-api.us-east-1.amazonaws.com/prod',
+    transactions: 'https://abcd1234.execute-api.us-east-1.amazonaws.com/prod',
+    // Si luego tienes transacciones o notificaciones, las agregas igual:
+    
+  }
+};
